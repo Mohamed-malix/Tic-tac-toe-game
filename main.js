baa85afa4efdc040bc7ec8a3213ed2a478878ff1
malix <288672436+Mohamed-malix@users.noreply.github.com>
2026-09-07 14:43:47 +0300

    added the winning conditions to the game

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
 
-// let span1 = document.querySelector('.span1')
-// let span2 = document.querySelector('.span2')
-// let span3 = document.querySelector('.span3')
-// let span4 = document.querySelector('.span4')
-// let span5 = document.querySelector('.span5')
-// let span6 = document.querySelector('.span6')
-// let span7 = document.querySelector('.span7')
-// let span8 = document.querySelector('.span8')
-// let span9 = document.querySelector('.span9')
-
-
-
 
 
 
@@ -28,7 +16,7 @@ function createPlayer(nameOf){
     }
 }
 
-let gameBoard=[];
+let gameBoard=['','','','','','','','',''];
 const player1 =createPlayer('player1');
 const player2 =createPlayer('player2');
 console.log(player2)
@@ -40,24 +28,97 @@ console.log(player2)
 function addxo(){
 
     let i=0;
-    while(i<5){
+    while(i<9){
+        let n=0;
 
         if(i%2==0){
-            let obj={};
+           if(gameBoard[1]=='X' && gameBoard[2]=='X' && gameBoard[3]=='X'){
+            console.log('X win');
+            break;
+           }
+           else if(gameBoard[4]=='X' && gameBoard[5]=='X' && gameBoard[6]==='X'){
+            console.log('X win');
+            break;
+           }
+            else if(gameBoard[7]=='X' && gameBoard[8]=='X' && gameBoard[9]==='X'){
+            console.log('X win');
+            break;
+           }
+           else if(gameBoard[1]=='X' && gameBoard[4]=='X' && gameBoard[7]==='X'){
+            console.log('X win');
+            break;
+           }
+           else if(gameBoard[2]=='X' && gameBoard[5]=='X' && gameBoard[8]==='X'){
+            console.log('X win');
+            break;
+           }
+           else if(gameBoard[3]=='X' && gameBoard[6]=='X' && gameBoard[9]==='X'){
+            console.log('X win');
+            break;
+           }
+           else if(gameBoard[1]=='X' && gameBoard[5]=='X' && gameBoard[9]==='X'){
+            console.log('X win');
+            break;
+           }
+           else if(gameBoard[3]=='X' && gameBoard[5]=='X' && gameBoard[7]==='X'){
+            console.log('X win');
+            break;
+           }
+           else{
+                n= Number(prompt("the place"));
+                gameBoard[n]='X';
+           }
+           
 
-            obj.type='X';
-            obj.place= Number(prompt('The place'));
         }
         else{
-            let obj={};
-            obj.type='O';
-            obj.place= Number(prompt('The place'));
+            if(i%2==0){
+           if(gameBoard[1]=='X' && gameBoard[2]=='X' && gameBoard[3]==='O'){
+            console.log('O win');
+            break;
+           }
+           else if(gameBoard[4]=='X' && gameBoard[5]=='X' && gameBoard[6]==='O'){
+            console.log('O win');
+            break;
+           }
+            else if(gameBoard[7]=='X' && gameBoard[8]==='X' && gameBoard[9]==='O'){
+            console.log('O win');
+            break;
+           }
+           else if(gameBoard[1]=='X' && gameBoard[4]=='X' && gameBoard[7]==='O'){
+            console.log('O win');
+            break;
+           }
+           else if(gameBoard[2]=='X' && gameBoard[5]=='X' && gameBoard[8]==='O'){
+            console.log('O win');
+            break;
+           }
+           else if(gameBoard[3]=='X' && gameBoard[6]=='X' && gameBoard[9]==='O'){
+            console.log('O win');
+            break;
+           }
+           else if(gameBoard[1]=='X' && gameBoard[5]=='X' && gameBoard[9]==='O'){
+            console.log('O win');
+            break;
+           }
+           else if(gameBoard[3]=='X' && gameBoard[5]=='X' && gameBoard[7]==='O'){
+            console.log('O win');
+            break;
+           }
+           else{
 
-            gameBoard.push(obj);
-            console.log(gameBoard);
+                n= Number(prompt("the place"));
+                gameBoard[n]='O';
+           }
+
+          }
+
+
+          if(i+1==9){
+            console.log('There is no winner');
+          }
+            i++;
         }
-
-        i++;
     }
 }
 
